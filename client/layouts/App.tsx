623e7e865afc5459542be4e6e6855e30a1a52668
Donghyun Lee <skyblue5030@naver.com>
2022-07-02 16:58:56 +0900

    폴더구조와 리액트 라우터 설정

--- client/layouts/App.tsx
+++ client/layouts/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import Login from '@pages/Login';
+import SignUp from '@pages/SignUp';
 
 const App = () => {
     return (
-        <div>
-            좀만 참아 주세요
-        </div>
+        <Switch>
+            <Redirect exact path='/' to="/login" />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+        </Switch>
     )
 };
 
