900e8fee724719e75201c0cb70eebe9951dc90f1
Donghyun Lee <skyblue5030@naver.com>
2022-07-02 15:33:01 +0900

    웹팩 설정: 주석으로 설명 달기

--- front/webpack.config.ts
+++ front/webpack.config.ts
@@ -17,6 +17,8 @@ const config: Configuration = {
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
+      // 절대 경로 설정 (.././ 이러한 경로를 없애줌)
+      // 앞에 표시는 @, ~, 생략이 가능하다. 맘에 드는 걸로
       '@hooks': path.resolve(__dirname, 'hooks'),
       '@components': path.resolve(__dirname, 'components'),
       '@layouts': path.resolve(__dirname, 'layouts'),
@@ -39,7 +41,7 @@ const config: Configuration = {
             [
               '@babel/preset-env',
               {
-                targets: { browsers: ['IE 10'] },
+                targets: { browsers: ['last 2 chrome versions'] },
                 debug: isDevelopment,
               },
             ],
@@ -72,22 +74,24 @@ const config: Configuration = {
     new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
   ],
   output: {
+    // yarn build 할 때의 처리
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
+    filename: '[name].js', // entry 에서 설정한 속성이 [name] 이에 담김
     publicPath: '/dist/',
   },
   devServer: {
-    historyApiFallback: true,
+    //개발 서버 관련
+    historyApiFallback: true, // 나중에 react-router 설정시 필요
     port: 3090,
     devMiddleware: { publicPath: '/dist/' },
     static: { directory: path.resolve(__dirname) },
-    proxy: {
-      '/api/': {
-        target: 'http://localhost:3095',
-        changeOrigin: true,
-        ws: true,
-      },
-    },
+    // proxy: {
+    //   '/api/': {
+    //     target: 'http://localhost:3095',
+    //     changeOrigin: true,
+    //     ws: true,
+    //   },
+    // },
   },
 };
 
@@ -102,6 +106,7 @@ if (isDevelopment && config.plugins) {
   );
   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: false }));
 }
+
 if (!isDevelopment && config.plugins) {
   config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
