192d48b697f75687c72e622047cfc37061dfe09c
Donghyun Lee <skyblue5030@naver.com>
2022-07-07 19:05:14 +0900

    useInput 타입 정의 수정

--- client/hooks/useInput.ts
+++ client/hooks/useInput.ts
@@ -1,12 +1,18 @@
-import { Dispatch, SetStateAction, useCallback, useState } from 'react';
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useState,
+  ChangeEvent,
+} from 'react';
 
-type Handler = (e: any) => void;
+type Handler = (e: ChangeEvent<HTMLInputElement>) => void;
 type ReturnTypes<T = any> = [T, Handler, Dispatch<SetStateAction<T>>];
 
 const useInput = <T = any>(initialValue: T): ReturnTypes<T> => {
   const [value, setValue] = useState(initialValue);
-  const handler = useCallback((e) => {
-    setValue(e.target.value);
+  const handler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value as unknown as T);
   }, []);
   return [value, handler, setValue];
 };
